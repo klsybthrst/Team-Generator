266a873a293fd4a68f075b3b5e3deb3f306ffcf4
kelseybathurst <kelsey.bathurst@gmail.com>
2020-10-18 19:50:25 -0400

    added app.js

--- Develop/app.js
+++ Develop/app.js
@@ -33,3 +33,74 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+// --- OUR CODE STARTS HERE ---
+
+let userList = []
+let userId = 1
+
+async function main(){
+    // get the project info and number of people to include
+    const managerInfo = await inquirer.prompt([
+        {
+            name: "name",
+            message: "What is the manager's Name?"
+        },
+        {
+            name: "email",
+            message: "What is the manager's Email?"
+        },
+        {
+            name: "office",
+            message: "What is the manager's OfficeNumber?"
+        },
+        {
+            name: "teamMembers",
+            message: "How many members in your team?"
+        }]
+    )
+
+    const manager = new Manager( managerInfo.name, userId++, managerInfo.email, managerInfo.office )
+    userList.push( manager )
+    // loop through and get user name/title for each person
+    for( var i=0; i<managerInfo.teamMembers; i++ ){
+        let employeeType = await inquirer.prompt([
+            {
+              type: "list",
+              name: "type",
+              message: "Employee type to add?",
+              choices: [
+                "Engineer",
+                "Intern"
+              ]
+            } ] )
+        employeeType = employeeType.type
+
+        const employeeInfo = await inquirer.prompt([
+            {
+                name: "name",
+                message: `What is the ${employeeType}'s Name?`
+            },
+            {
+                name: "email",
+                message: `What is the ${employeeType}'s Email?`
+            },            
+            {
+                name: "info", // this is either 'github' or 'school' depending on employeeType
+                message: `What is the ${employeeType}'s ${employeeType=='Engineer' ? 'GitHub' : 'School'}?`
+            } ])
+
+        const employee = employeeType==='Engineer' 
+            ? new Engineer( employeeInfo.name, userId++, employeeInfo.email, employeeInfo.info )
+            : new Intern( employeeInfo.name, userId++, employeeInfo.email, employeeInfo.info )
+        userList.push( employee )
+    }
+
+    // Create the output directory if the output path doesn't exist
+    if( !fs.existsSync(OUTPUT_DIR) ) fs.mkdirSync(OUTPUT_DIR)
+    // use the pre-built render function to build the list...
+    fs.writeFileSync(outputPath, render(userList), "utf-8");
+
+    console.log( `Completed writing to: ${outputPath}` )
+}
+main()
